0c19da6c9849b50da7c326f0caee8138e78ce528
zhangxin <36006342+FHamster@users.noreply.github.com>
2019-12-13 11:23:54 +0800

    bug 最新，搜索无法匹配

--- Final_NOTE/Form1.cs
+++ Final_NOTE/Form1.cs
@@ -17,7 +17,9 @@ namespace NOTE
     public partial class Note : Form
     {
         MyDrawBox myDrawBox;
-
+        string[] list= new string[100];//存储搜索源
+        int index = 0;//list的下标
+        Boolean search = false;//textbox的功能
         public Note()
         {
             InitializeComponent();
@@ -26,14 +28,15 @@ namespace NOTE
             penSize.Items.Add(7);
             penSize.Items.Add(9);
             penSize.Items.Add(11);
-
-            var list = GetTestList();
-            //搜索匹配
-            this.textBox1.AutoCompleteCustomSource.Clear();
-            this.textBox1.AutoCompleteCustomSource.AddRange(list.ToArray());
-            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-
+            this.textBox1.Visible = false;
+            //初始生成10个笔记
+            for (int i = 0; i < 10; i++)
+            {
+                this.NoteList.Items.Add("笔记" + (i+1));
+                list[i] = "笔记" + (i+1);
+                index = i;
+            }
+            
             //for(int i = 0; NoteList.SelectedIndex != -1; i++)
             //{
             //    if (this.textBox1.Text == NoteList.SelectedItem.ToString())
@@ -84,26 +87,36 @@ namespace NOTE
             System.Drawing.Font objFonts = new Font(FontBox.Items[e.Index].ToString(), 14);
             e.ItemHeight = objFonts.Height;
         }
-
+        //新增笔记本--名字
+        Boolean noteName = false;
         private void 新增ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.NoteList.Items.Add("k1");
+            noteName = true;
+            this.textBox1.Visible = true;
+            
         }
         //测试数据
-        private List<string> GetTestList()
-        {
-            var list = new List<string>();
-            for (int i = 0; i < 10; i++)
-            {
-                list.Add("笔记" + (i + 1));
-            }
-            return list;
-        }
+        //private List<string> GetTestList()
+        //{
+        //    var list = new List<string>();
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        list.Add("笔记" + (i + 1));
+        //    }
+        //    return list;
+        //}
         private void button1_Click_1(object sender, EventArgs e)
         {
-            var list = GetTestList();
-            this.NoteList.DataSource = list;
-            this.NoteList.ToString();
+            //var list = GetTestList();
+            //for(int i = 0;i<10 ; i++)
+            //{
+            //    this.NoteList.Items.Add("笔记" + i);
+            //    list[i] = "笔记"+i;
+            //}
+            //this.textBox1.Visible = true;
+            //this.NoteList.Items.Add(this.textBox1.Text);
+            //index++;
+            //list[index] = this.textBox1.Text;
         }
 
         private void NoteList_MouseClick(object sender, MouseEventArgs e)
@@ -151,24 +164,50 @@ namespace NOTE
 
             if (e.KeyCode == Keys.Enter)
             {
-                for (int i = 0; i < this.NoteList.Items.Count; i++)
+                
+                if (search)//此时textbox进行搜索功能
                 {
-                    if (this.textBox1.Text == this.NoteList.Items[i].ToString())
+                 
+                    for (int i = 0; i < this.NoteList.Items.Count; i++)
                     {
-                        //this.NoteList.SelectedIndex = i;
+                        if (this.textBox1.Text == this.NoteList.Items[i].ToString())
+                        {
+                            //this.NoteList.SelectedIndex = i;
 
-                        MessageBox.Show("按下了回车键,找到" + this.NoteList.Items[i].ToString());
-                        Color vColor = Color.Gainsboro;
+                            MessageBox.Show("按下了回车键,找到" + this.NoteList.Items[i].ToString());
+                            Color vColor = Color.Gainsboro;
 
-                        Graphics devcolor = NoteList.CreateGraphics();
-                        vColor = Color.LightPink;
-                        devcolor.FillRectangle(new SolidBrush(vColor), NoteList.GetItemRectangle(i));
-                        devcolor.DrawString(NoteList.Items[i].ToString(), NoteList.Font, new SolidBrush(NoteList.ForeColor), NoteList.GetItemRectangle(i));
+                            Graphics devcolor = NoteList.CreateGraphics();
+                            vColor = Color.LightPink;
+                            devcolor.FillRectangle(new SolidBrush(vColor), NoteList.GetItemRectangle(i));
+                            devcolor.DrawString(NoteList.Items[i].ToString(), NoteList.Font, new SolidBrush(NoteList.ForeColor), NoteList.GetItemRectangle(i));
+                        }
                     }
+                }else if (noteName)
+                {
+                    this.NoteList.Items.Add(this.textBox1.Text);//listbox添加
+                    index++;
+                    list[index] = this.textBox1.Text;//list添加
+                    this.textBox1.Text = "";
+                    //搜索匹配
+                    this.textBox1.AutoCompleteCustomSource.Clear();
+                    this.textBox1.AutoCompleteCustomSource.AddRange(list);
+                    this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+                    this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+                    
                 }
 
 
+
             }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            search = true;
+            //显示搜索框
+            this.textBox1.Visible = true;
+            
         }
     }
 }
